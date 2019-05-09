5deb219ed262dd172357fe7ddab96eb4b1a05da4
hly99 <1055503066@qq.com>
2019-05-09 18:20:06 +0800

    Create App.vue

---
+++ Vue-2048/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <root />
+  </div>
+</template>
+
+<script>
+import root from './components/root.vue'
+
+export default {
+  name: 'app',
+  components: {
+    root
+  }
+}
+</script>
